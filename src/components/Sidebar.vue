b7ee4fa5e8eda8ac009646d02f41d7bb460f8c73
patmood <patrick.n.moody@gmail.com>
2017-06-22 22:09:11 -0700

    initial commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <aside>
+    <ol>
+      <li v-for="post in posts" :key="post.data.id">
+        {{ post.data.title }}
+      </li>
+    </ol>
+  </aside>  
+</template>
+
+
+<script>
+export default {
+  name: 'sidebar',
+  data () {
+    return {
+      posts: [],
+    }
+  },
+  created: function () {
+    fetch(`https://www.reddit.com/.json`)
+      .then(res => res.json())
+      .then(json => { this.posts = json.data.children })
+  },
+}
+</script>
