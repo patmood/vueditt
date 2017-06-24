e9d25aceaeeb724e3675705ff217ae0acaf0ee60
patmood <patrick.n.moody@gmail.com>
2017-06-23 21:08:34 -0700

    style sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,30 @@
 <template>
-  <aside :style="{width: '300px'}" class="flex-none">
-    <ol>
-      <li v-for="post in posts" :key="post.data.id">
-        <router-link :to="{ name: 'Comments', params: { id: post.data.id }}">
-          {{ post.data.title }}
+  <aside :style="{width: '300px'}" class="flex-none border-right border-gray">
+    <div class="flex items-center p1">
+      <router-link :to="{ name: 'Hello' }">Home</router-link>
+      <div class="flex-auto center">
+        {{ title }}
+      </div>
+    </div>
+    <ol class="list-reset p0 m0">
+      <li
+        v-for="post in posts"
+        :key="post.data.id"
+        class="flex border-bottom border-gray hover"
+      >
+        <div class="flex-auto p1">
+          <div>
+            {{ post.data.title }}
+          </div>
+          <div class="muted">
+            {{ post.data.domain.toLowerCase() }}
+          </div>
+        </div>
+        <router-link
+          :to="{ name: 'Comments', params: { id: post.data.id }}"
+          class="center text-decoration-none color-inherit muted bg-darken-1"
+        >
+          {{ post.data.num_comments }}
         </router-link>
       </li>
     </ol>
@@ -18,6 +39,12 @@ export default {
     return {
       posts: [],
     }
+  },
+  computed: {
+    title: function () {
+      const subreddit = this.$route.query.subreddit
+      return subreddit || 'Front page'
+    },
   },
   created: function () {
     const subreddit = this.$route.query.subreddit
